30c9a65857bcf8453f48aec84860960b87563ab6
tanjib10 <radiulmd@gmail.com>
2023-12-01 17:48:53 +0600

    added conditional data rendering method

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
    //users api
    app.post('/users', async (req,res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+         return res.send({message: "user exists", insertedId: null}) 
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
    })
